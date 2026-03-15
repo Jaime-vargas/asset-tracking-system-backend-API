b653d2de01f7cebd19bafa72a65af05a7de11776
Jaime-vargas <jaime.vargas@live.com.mx>
2026-03-14 21:14:09 -0600

    dto branch

--- src/main/java/com/control_activos/sks/control_activos/models/dto/branchDTO/BranchTableDTO.java
+++ src/main/java/com/control_activos/sks/control_activos/models/dto/branchDTO/BranchTableDTO.java
@@ -1,4 +1,20 @@
 package com.control_activos.sks.control_activos.models.dto.branchDTO;
 
+import com.control_activos.sks.control_activos.models.dto.reportDTO.ReportCountDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BranchTableDTO {
+    Long id;
+    String name;
+    Long totalHardware;
+    List<ReportCountDTO> reportsActive;
 }
