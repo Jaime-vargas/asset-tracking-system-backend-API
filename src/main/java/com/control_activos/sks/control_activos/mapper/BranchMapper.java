61841a7f55e06beba93628dad9da026d7ba73e0c
Jaime-vargas <jaime.vargas@live.com.mx>
2026-03-14 21:13:02 -0600

    class mapper for dto table

--- src/main/java/com/control_activos/sks/control_activos/mapper/BranchMapper.java
+++ src/main/java/com/control_activos/sks/control_activos/mapper/BranchMapper.java
@@ -1,4 +1,15 @@
-package com.control_activos.sks.control_activos.mapper.branchMapper;
+package com.control_activos.sks.control_activos.mapper;
+
+import com.control_activos.sks.control_activos.models.dto.branchDTO.BranchTableDTO;
+import com.control_activos.sks.control_activos.models.entity.Branch;
 
 public class BranchMapper {
+
+    public static BranchTableDTO toBranchTableDTO(Branch branch) {
+        BranchTableDTO branchTableDTO = new BranchTableDTO();
+        branchTableDTO.setId(branch.getId());
+        branchTableDTO.setName(branch.getName());
+        branchTableDTO.setTotalHardware((long) branch.getHardware().size());
+        return branchTableDTO;
+    }
 }
