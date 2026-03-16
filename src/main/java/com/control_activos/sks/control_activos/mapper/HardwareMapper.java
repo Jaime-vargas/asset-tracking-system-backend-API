96e8ea7b9b88b4d9cb9cc0263992a36ee14177f0
Jaime-vargas <jaime.vargas@live.com.mx>
2026-03-15 21:07:29 -0600

    create new dto for hardware data

--- src/main/java/com/control_activos/sks/control_activos/mapper/HardwareMapper.java
+++ src/main/java/com/control_activos/sks/control_activos/mapper/HardwareMapper.java
@@ -1,4 +1,20 @@
 package com.control_activos.sks.control_activos.mapper;
 
+import com.control_activos.sks.control_activos.models.dto.hardwareDTO.HardwareTableDTO;
+import com.control_activos.sks.control_activos.models.entity.Hardware;
+
+import java.util.Optional;
+
 public class HardwareMapper {
+
+    public static HardwareTableDTO toHardwareTableDTO(Hardware hardware) {
+        return new HardwareTableDTO(
+            hardware.getId(),
+            hardware.getClass().getSimpleName(),
+            hardware.getName(),
+            hardware.getModel(),
+            hardware.getSerialNumber(),
+            Optional.ofNullable(hardware.getLastMaintenanceDate()).map(Object::toString).orElse("N/A")
+        );
+    }
 }
--- src/main/java/com/control_activos/sks/control_activos/models/dto/hardwareDTO/HardwareTableDTO.java
+++ src/main/java/com/control_activos/sks/control_activos/models/dto/hardwareDTO/HardwareTableDTO.java
@@ -1,4 +1,27 @@
 package com.control_activos.sks.control_activos.models.dto.hardwareDTO;
 
+import com.control_activos.sks.control_activos.models.dto.reportDTO.ReportCountDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
 public class HardwareTableDTO {
+    Long id;
+    String type;
+    String name;
+    String model;
+    String serialNumber;
+    String lastMaintenanceDate;
+    List<ReportCountDTO> reportsActive;
+
+    public HardwareTableDTO(Long id, String type, String name, String model, String serialNumber, String lastMaintenanceDate) {
+        this.id = id;
+        this.type = type;
+        this.name = name;
+        this.model = model;
+        this.serialNumber = serialNumber;
+        this.lastMaintenanceDate = lastMaintenanceDate;
+    }
 }
