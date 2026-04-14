0c9ba0ca82935fd3a8684dddf60b2f5fc0dce0a7
Jaime-vargas <jaime.vargas@live.com.mx>
2026-04-13 21:00:23 -0600

    created void photo controller

--- src/main/java/com/control_activos/sks/control_activos/controller/PhotoController.java
+++ src/main/java/com/control_activos/sks/control_activos/controller/PhotoController.java
@@ -1,4 +1,22 @@
 package com.control_activos.sks.control_activos.controller;
 
+import com.control_activos.sks.control_activos.models.dto.PhotoDTO;
+import com.control_activos.sks.control_activos.services.FilesService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/photos")
 public class PhotoController {
+
+    private final FilesService filesService;
+
+    public PhotoController(FilesService filesService) {
+        this.filesService = filesService;
+     }
+
 }
