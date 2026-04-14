6a75ab6ee1928751925ce07c2c644147d0e2fce3
Jaime-vargas <jaime.vargas@live.com.mx>
2026-04-13 21:03:23 -0600

    camera mapper to convert to entity

--- src/main/java/com/control_activos/sks/control_activos/mapper/CameraMapper.java
+++ src/main/java/com/control_activos/sks/control_activos/mapper/CameraMapper.java
@@ -1,4 +1,21 @@
 package com.control_activos.sks.control_activos.mapper;
 
+import com.control_activos.sks.control_activos.models.dto.hardwareDTO.CameraRequestDTO;
+import com.control_activos.sks.control_activos.models.entity.Branch;
+import com.control_activos.sks.control_activos.models.entity.Camera;
+
 public class CameraMapper {
+
+    public static Camera toCameraEntity(CameraRequestDTO cameraRequestDTO, Branch branch) {
+        Camera camera = new Camera();
+        camera.setName(cameraRequestDTO.getName());
+        camera.setSerialNumber(cameraRequestDTO.getSerialNumber());
+        camera.setModel(cameraRequestDTO.getModel());
+        camera.setLocation(cameraRequestDTO.getLocation());
+        camera.setBranch(branch);
+        camera.setCameraId(cameraRequestDTO.getCameraId());
+        camera.setMacAddress(cameraRequestDTO.getMacAddress());
+        camera.setIpAddress(cameraRequestDTO.getIpAddress());
+        return camera;
+    }
 }
