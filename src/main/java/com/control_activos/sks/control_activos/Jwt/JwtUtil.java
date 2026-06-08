52d94251988a1ec3e019e3e4a49f1645a9115813
Jaime-vargas <jaime.vargas@live.com.mx>
2026-06-07 19:02:48 -0600

    added class in order to test jwt dependency

--- src/main/java/com/control_activos/sks/control_activos/Jwt/JwtUtil.java
+++ src/main/java/com/control_activos/sks/control_activos/Jwt/JwtUtil.java
@@ -1,4 +1,48 @@
 package com.control_activos.sks.control_activos.Jwt;
 
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
 public class JwtUtil {
+
+
+    private final SecretKey secretKey;
+    public JwtUtil() {
+        this.secretKey = Keys.hmacShaKeyFor(
+                "0cRJNYRU8UxYn5eZ0D6SPwHmRHsnpiIB".getBytes(StandardCharsets.UTF_8)
+        );
+    }
+
+    public String generateToken (Long id){
+
+        // Test Calims
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("Device ID", id);
+        claims.put("Type", "camera");
+
+        return Jwts.builder()
+                .claims(claims)
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public Long validateToken(String token){
+         Claims claims = Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+
+         return claims.get("Device ID", Long.class);
+    }
+
 }
